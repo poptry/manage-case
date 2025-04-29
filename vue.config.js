42f47f269cda6d76148a03eff258ded932b283e1
hhb <1123595386@qq.com>
2025-04-29 13:26:28 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,12 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  devServer: {
+    host: "localhost",
+    port: 8080,
+    https: false, // 禁用 HTTPS
+    hot: true,
+    historyApiFallback: true,
+    allowedHosts: "all",
+  },
 });
