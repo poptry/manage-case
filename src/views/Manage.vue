68e5748f04338e5ec8adf45453eabbfdb1917f16
hhb <1123595386@qq.com>
2025-04-29 12:36:07 +0800

    图表信息

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -1,25 +1,32 @@
 <template>
-  <div class="tableBox">
-    <!-- 表格 -->
-    <a-table :columns="columns" :data-source="data" row-key="id" bordered>
-      <!-- 自定义操作列 -->
-      <template #bodyCell="{ column, record }">
-        <template v-if="column.key === 'actions' && isAdmin">
-          <a-popconfirm title="确定要删除吗？" @confirm="() => handleDelete(record.id)">
-            <a-button type="link" danger>删除</a-button>
-          </a-popconfirm>
+  <div class="container">
+    <div class="tableBox">
+      <h1>Tasks</h1>
+      <!-- 表格 -->
+      <a-table :columns="columns" :data-source="data" row-key="id" bordered>
+        <!-- 自定义操作列 -->
+        <template #bodyCell="{ column, record }">
+          <template v-if="column.key === 'actions' && isAdmin">
+            <a-popconfirm title="确定要删除吗？" @confirm="() => handleDelete(record.id)">
+              <a-button type="link" danger>删除</a-button>
+            </a-popconfirm>
+          </template>
+          <template v-else-if="column.key === 'overtime'">
+            <span>{{ record.overtime ? '是' : '否' }}</span>
+          </template>
         </template>
-        <template v-else-if="column.key === 'overtime'">
-          <span>{{ record.overtime ? '是' : '否' }}</span>
-        </template>
-      </template>
-    </a-table>
+      </a-table>
+    </div>
+    <div class="graphBox">
+      <div style="width: 100%; height: 400px;" ref="barChart"></div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
 import { getProjectList, deleteProjectById } from "@/api";
+import * as echarts from "echarts";
 const data = ref([]);
 // 判断是否为管理员
 const role = localStorage.getItem("userRole");
@@ -36,10 +43,48 @@ const columns = [
 if (isAdmin) {
   columns.push({ title: "actions", key: "actions" });
 }
+// 创建柱状图
+const barChart = ref(null);
+const initChart = () => {
+  const chartInstance = echarts.init(barChart.value);
+  console.log(data.value);
+
+  const xData = data.value.map((item) => item.project); // X轴数据
+  const yData = data.value.map((item) => item.hours); // Y轴数据
+  console.log(xData, yData);
+  const option = {
+    title: {
+      text: "Project Hours Distribution",
+    },
+    tooltip: {},
+    xAxis: {
+      type: "category",
+      data: xData, // X轴数据
+      axisLabel: {
+        rotate: 45, // 标签旋转45度
+        interval: 0, // 强制显示所有标签
+      },
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: yData, // Y轴数据
+        type: "bar",
+        itemStyle: {
+          color: "#4fa3f7",
+        },
+      },
+    ],
+  };
+  chartInstance.setOption(option);
+};
 // 模拟请求数据
-const fetchData = () => {
-  getProjectList().then((res) => {
+const fetchData = async () => {
+  await getProjectList().then((res) => {
     data.value = res.data.data;
+    initChart();
   });
 };
 // 删除处理
@@ -55,10 +100,18 @@ onMounted(() => {
 });
 </script>
 <style lang="less" scoped>
-.tableBox {
-  width: 50%;
-  margin: 20px;
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
   padding: 20px;
-  border: 1px solid #ccc;
+  margin: 20px;
+  .tableBox {
+    width: 60%;
+  }
+  .graphBox {
+    width: 40%;
+    margin-left: 20px;
+  }
 }
 </style>
